3117c0b9cc46fe049dda0c9a5a023ce60c91bbf1
Andrés Del Carpio <andresdc@andresdc.net>
2020-11-23 11:23:36 -0500

    First commit

---
+++ src/components/Background.vue
@@ -0,0 +1,43 @@
+<template>
+  {{ color.slice(1).toUpperCase() }}
+</template>
+<script>
+const values = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+];
+
+export default {
+  data() {
+    return {
+      color: '',
+    };
+  },
+  methods: {
+    getRandom() {
+      return values[Math.floor(Math.random() * values.length)];
+    },
+    generateColorHex() {
+      return `#${this.getRandom()}${this.getRandom()}${this.getRandom()}${this.getRandom()}${this.getRandom()}${this.getRandom()}`;
+    },
+  },
+  mounted() {
+    this.color = this.generateColorHex();
+    document.body.style.backgroundColor = this.color;
+  },
+};
+</script>
