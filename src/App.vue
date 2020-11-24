d3bc940c82be53641e3a61a41ea301d6592020a1
Andrés Del Carpio <andresdc@andresdc.net>
2020-11-24 12:32:50 -0500

    Second commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,57 @@
 <template>
-  <Background />
+  <Information :colorHex="colorHex" :rgb="hexToRGB(colorHex)" />
 </template>
 
 <script>
-import Background from './components/Background.vue';
+import Information from './components/Information.vue';
+
+const values = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+];
 
 export default {
   name: 'App',
+  data() {
+    return {
+      colorHex: '',
+    };
+  },
   components: {
-    Background,
+    Information,
+  },
+  methods: {
+    getRandom() {
+      return values[Math.floor(Math.random() * values.length)];
+    },
+    generateColorHex() {
+      return `${this.getRandom()}${this.getRandom()}${this.getRandom()}${this.getRandom()}${this.getRandom()}${this.getRandom()}`;
+    },
+    hexToRGB(hex) {
+      let rgb = hex.match(/.{1,2}/g);
+      return `R: ${parseInt(rgb[0], 16)}, G: ${parseInt(
+        rgb[1],
+        16
+      )}, B: ${parseInt(rgb[2], 16)}`;
+    },
+  },
+  beforeMount() {
+    this.colorHex = this.generateColorHex();
+    document.body.style.backgroundColor = `#${this.colorHex}`;
   },
 };
 </script>
---
+++ src/components/Information.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="information">
+    <table>
+      <tbody>
+        <tr>
+          <td>Hex</td>
+          <td>{{ colorHex.toUpperCase() }}</td>
+        </tr>
+        <tr>
+          <td>RGB</td>
+          <td>{{ rgb }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Information',
+  props: ['colorHex', 'rgb'],
+};
+</script>
+
+<style lang="scss">
+.information {
+  width: 20%;
+  background: white;
+  opacity: 0.5;
+
+  table {
+    width: 100%;
+    text-align: center;
+  }
+}
+</style>
